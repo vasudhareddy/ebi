affb3a4b96b19abdc1125fbf2a5a940eea29ddb9
vasudhareddy <karri.vasudha@gmail.com>
2021-03-20 10:13:58 +0530

    moved datepattern to application.properties

--- src/main/java/com/embl/assignment/controllers/DateController.java
+++ src/main/java/com/embl/assignment/controllers/DateController.java
@@ -1,6 +1,7 @@
 package com.embl.assignment.controllers;
 
 import com.embl.assignment.model.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +11,12 @@ import java.time.format.*;
 @RestController
 public class DateController {
 
+    @Value("${datepattern}")
+    String datePattern;
+
     @GetMapping(value = "/date", produces = MediaType.APPLICATION_JSON_VALUE)
     public DateResponse fetchDate() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss z yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(datePattern);
         String format = formatter.format(ZonedDateTime.now());
         DateResponse dateResponse = new DateResponse(format);
         return dateResponse;
